bac039b64b04007089002a387df003fb673de060
Dan McClain <git@danmcclain.net>
2013-01-25 13:46:15 -0500

    Gives the generator a proper test

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -11,10 +11,16 @@ class PartyFoul::GeneratorTest < Rails::Generators::TestCase
     oauth.expects(:create).with('scopes' => ['repo'])
     github = mock('Github')
     github.stubs(:oauth).returns(oauth)
-    github.stubs(:oauth_token).returns('')
+    github.stubs(:oauth_token).returns('test_token')
     Github.stubs(:new).with(:login => 'test_login', :password => 'test_password', :endpoint => 'test_endpoint').returns(github)
-    $stdin.stubs(:gets).returns('test_login').then.returns('test_password').then.returns('').then.returns('test_repo').then.returns('test_endpoint')
+    $stdin.stubs(:gets).returns('test_login').then.returns('test_password').then.returns('').then.returns('test_repo').then.returns("test_endpoint").then.returns('')
     run_generator
-    assert_file 'config/initializers/party_foul.rb'
+
+    assert_file 'config/initializers/party_foul.rb' do |initializer|
+      assert_match(/config.endpoint\s+=\s'test_endpoint'/, initializer)
+      assert_match(/config.owner\s+=\s'test_login'/, initializer)
+      assert_match(/config.repo\s+=\s'test_repo'/, initializer)
+      assert_match(/config.oauth_token\s+=\s'test_token'/, initializer)
+    end
   end
 end
