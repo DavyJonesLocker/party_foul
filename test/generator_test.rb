24f394feafd25c5d68f4674f007f4135d9ec3fc7
Dan McClain <git@danmcclain.net>
2013-01-25 12:26:41 -0500

    Tests generator

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -6,7 +6,14 @@ class PartyFoul::GeneratorTest < Rails::Generators::TestCase
   destination File.expand_path('../tmp', __FILE__)
   tests PartyFoul::InstallGenerator
   test 'it copies the initializer' do
-    $stdin.stubs(:gets).returns('')
+    oauth = mock('Oauth')
+    oauth.stubs(:create)
+    oauth.expects(:create).with('scopes' => ['repo'])
+    github = mock('Github')
+    github.stubs(:oauth).returns(oauth)
+    github.stubs(:oauth_token).returns('')
+    Github.stubs(:new).with(:login => 'test_login', :password => 'test_password', :endpoint => 'test_endpoint').returns(github)
+    $stdin.stubs(:gets).returns('test_login').then.returns('test_password').then.returns('').then.returns('test_repo').then.returns('test_endpoint')
     run_generator
     assert_file 'config/initializers/party_foul.rb'
   end
